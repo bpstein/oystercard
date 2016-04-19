5ae43360469cd17af9f9c4059a41ba08cb200234
omajul <omajul2@hotmail.com>
2016-04-19 10:09:55 +0000

    Step 4 - Add a balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,12 @@
-require 'Oystercard'
+require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard) { described_class.new }
+
+  context "when initialized" do
+    it "has a balance equals to 0" do
+      expect(oystercard.balance).to eq 0
+    end
+  end
 
 end
