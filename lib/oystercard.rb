c8ab0e4ec4a5b51136c7d9a1096b713666f08f5c
omajul <omajul2@hotmail.com>
2016-04-19 10:33:47 +0000

    Step 6 - Add limit for maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,13 @@
 class Oystercard
+  MAXIMUM_BALANCE = 90 
   attr_reader :balance
-
+  
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    raise "Cannot exceed the limit of #{MAXIMUM_BALANCE}" if @balance + amount > MAXIMUM_BALANCE
     @balance += amount
   end
 end
