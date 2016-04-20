006dddad997dd44647fb44388c25b4f320841379
Ben Stein <bumstain@gmail.com>
2016-04-21 01:55:45 +1000

    Draft tests for journey_spec

---
+++ spec/journey_spec.rb
@@ -0,0 +1,29 @@
+require 'oystercard'
+
+describe Oystercard do
+
+end
+
+
+# retain touch_in and touch_out on oystercard
+
+
+# oystercard.touch_in(station)
+# oystercard.touch_out(station)
+# expect(journey).to not be nil 
+
+
+
+# oystercard.touch_in(station)
+# expect {(oystercard.touch_in(station))}.to raise_error "You must touch out first before touching in"
+
+
+
+# oystercard.touch_out(station)
+# expect {(oystercard.touch_out(station))}.to raise_error "You must touch in first before touching out"
+
+
+
+# oystercard.touch_in(station)
+# oystercard.touch_out(station)
+# expect(journey).to not be nil 
